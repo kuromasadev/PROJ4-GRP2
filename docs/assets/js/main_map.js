14cb5d3da13b04be9990818d69ce44a6fe367288
kuromasadev <kuromasadev@gmail.com>
2023-09-04 10:46:53 -0500

    WEBSITE - Maps
    Co-Authored-By: doaabarq <126979650+doaabarq@users.noreply.github.com>

--- docs/assets/js/main_map.js
+++ docs/assets/js/main_map.js
@@ -7,7 +7,7 @@ let config = {
   ]
 }
 // magnification with which the map will start
-const zoom = 6;
+const zoom = 10;
 // coordinates
 const lat = 29.749907;
 const lng = -95.358421;
@@ -16,36 +16,36 @@ const lng = -95.358421;
 const map = L.map("map", config).setView([lat, lng], zoom);
 
 
-fetch('../geojson/01_biz_bldgs.geojson')
-  .then(response => response.json())
-  .then(data => {
-    // Create the marker cluster group
-    let markers = L.markerClusterGroup();
+// Fetch and parse CSV data using PapaParse
+fetch('../../assets/geojson/1_dist.csv')
+  .then(response => response.text())
+  .then(csvText => {
+    Papa.parse(csvText, {
+      header: true,
+      complete: function(results) {
+        results.data.forEach(row => {
+          const lat = parseFloat(row.Latitude); 
+          const lng = parseFloat(row.Longitude);
 
-    // Add markers to the layer
-    data.features.forEach(feature => {
-      if (feature.geometry && feature.geometry.coordinates) {
-        const [lng, lat] = feature.geometry.coordinates;
-
-        // Create individual markers
-        let marker = L.marker([lat, lng]);
-
-        // Add markers to the marker cluster group
-        markers.addLayer(marker);
+          if (!isNaN(lat) && !isNaN(lng)) {
+            const marker = L.marker([lat, lng]).addTo(map);
+          }
+        });
       }
     });
-
-    // Add the marker cluster group to the map
-    map.addLayer(markers);
   })
   .catch(error => {
-    console.error('Error fetching or processing GeoJSON:', error);
-});
+    console.error('Error fetching or processing CSV:', error);
+  });
 
+
+
+
+const service1_Url = 'https://services.arcgis.com/ue9rwulIoeLEI9bj/arcgis/rest/services/US_StateBoundaries/FeatureServer/0';
 
 // Adding the US boundary 
-let serviceLayer = L.esri.featureLayer({
-  url: 'https://gisportal.ers.usda.gov/server/rest/services/Atlas_Reference_Data/Reference_Labels/MapServer/1',
+let serviceLayer1 = L.esri.featureLayer({
+  url: service1_Url,
   style: function(feature) {
     return {
       color: '#000', 
@@ -56,41 +56,37 @@ let serviceLayer = L.esri.featureLayer({
 }
 }).addTo(map);
 
+const service2_Url = 'https://services.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services/usa_cbsa/FeatureServer/0';
 
-// Adding the US METRO areas only when zoomed in at 6
-let geojsonLayer; 
-
-map.on('zoomend', function() {
-    var zoomlevel = map.getZoom();
-
-    // Load the data if the zoom level is 6 or greater
-    if (zoomlevel >= 6) {
-        if (!geojsonLayer) {  // load the data only if it hasn't been loaded yet
-            fetch('./flaskapp/data/new_metrodata.geojson')
-            .then(response => response.json())
-            .then(data => {
-              console.log(data)
-                // Define the style for the layer
-                let layerStyle = feature => ({
-                    stroke: false,
-                    fillColor: feature.properties.Metro2013 == 1 ? '#f00' : '#0f0',
-                    fillOpacity: 0.5
-                });
-
-                geojsonLayer = L.geoJSON(data, { style: layerStyle });  
-                geojsonLayer.addTo(map);
-            })
-            .catch(error => console.error('Fetch Error:', error));
-        }
+// Adding the US Metro Areas
+let serviceLayer2 = L.esri.featureLayer({
+  url: service2_Url,
+  style: function(feature) {
+    const cbsaType = feature.properties.CBSA_TYPE;
+    if (cbsaType === 'Metropolitan') {
+      return {
+        color: '#ff0000',   // Red color for Metropolitan features
+        weight: 2,
+        opacity: 0.35,
+        fill: true
+      };
+    } else if (cbsaType === 'Micropolitan') {
+      return {
+        color: '#2A7221',   // Green color for Micropolitan features
+        weight: 0.5,
+        opacity: 1,
+        fill: true
+      };
+    } else {
+      return {
+        color: '#000',      // Default color for other features
+        weight: 1,
+        opacity: 1,
+        fill: false
+      };
     }
-    // Remove the data if the zoom level is less than 6
-    else {
-        if (geojsonLayer) {  // check if the layer exists
-            map.removeLayer(geojsonLayer);  
-            geojsonLayer = null;  
-        }
-    }
-});
+  }
+}).addTo(map);
 
 
 // Adding the tile layer
